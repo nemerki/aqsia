772a347fd5a57a08b8aeebb65be1da36e6b94e4c
nemerki <mmehemmedm@gmail.com>
2018-04-25 00:57:47 +0400

    calendar oluşturulmaya baslandi

--- build/js/all.js
+++ build/js/all.js
@@ -1,3 +1,9 @@
+$(function() {
+  $( "#datepicker" ).datepicker({ firstDay: 1, minDate: 0 });
+});
+
+// minDate = unselected days before current day
+// delete ", minDate: 0" for all days
 $(document).ready(function() {
 	
 });
