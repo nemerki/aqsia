c5322ae72f850aa68f201f458a928a6322cc7196
nemerki <mmehemmedm@gmail.com>
2018-04-27 18:05:06 +0400

    menu hazir gibi

--- src/js/main/owlcarusel.js
+++ src/js/main/owlcarusel.js
@@ -16,5 +16,41 @@ $('.uzvluk-qaydalari__carusel').owlCarousel({
 $('.secilmis-mehsullar__carusel').owlCarousel({
 	loop:true,
 	items:4,
+	autoplay:true,
+	margin: 24,
+	dots: false,
+
+	responsive:{
+		0:{
+			items:1,
+			nav:false
+		},
+
+		578:{
+			items:2,
+			nav:false
+		},
+		768:{
+			items:3,
+			loop:false
+		},
+		992:{
+			items:4,
+			loop:false
+		}
+	}
+})
+
+$('.index-slide__left-carusel').owlCarousel({
+	loop:true,
+	items:1,
 	autoplay:true
+})
+
+$('.index-slide__right-carusel').owlCarousel({
+	loop:true,
+	items:1,
+	nav:true,
+	autoplay:true
+
 })
